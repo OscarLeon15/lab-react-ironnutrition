55e45c419f29fe2bf85c29c84ac84daa7b107560
oscar leon <oscar.leon.delmar@gmail.com>
2019-09-27 22:17:04 -0500

    review

--- starter-code/src/components/FoodBox.js
+++ starter-code/src/components/FoodBox.js
@@ -1,31 +1,25 @@
+
 import React from 'react';
-import food from '../foods.json'
-import 'bulma/css/bulma.css';
+
+
 
 class FoodBox extends React.Component{
-    constructor(props){
-        super(props);
 
-        this.state={
-            foodList: food,
-        }
-    }
+    render(){
+        return(
 
-    showFood = () => {
-        return this.state.foodList.map((eachFood, index)=>{
-            return(
-                <div className="box" style={{width:"600px", margin:"10px"}}>
-  <article key={index} className="media">
+<div className="box">
+  <article className="media">
     <div className="media-left">
       <figure className="image is-64x64">
-        <img src={eachFood.image} />
+        <img src={this.props.theImage}  className="img-size"/>
       </figure>
     </div>
     <div className="media-content">
       <div className="content">
         <p>
-          <strong>{eachFood.name}</strong> <br />
-          <small>{eachFood.calories}</small>
+          <strong>{this.props.theName}</strong> <br />
+          <small>{this.props.theCalories} cal</small>
         </p>
       </div>
     </div>
@@ -35,51 +29,123 @@ class FoodBox extends React.Component{
           <input
             className="input"
             type="number" 
-            value={eachFood.quantity}
+          
           />
         </div>
         <div className="control">
           <button className="button is-info">
-          +
+            +
           </button>
         </div>
       </div>
     </div>
   </article>
 </div>
-            )
-        })
+          
+        )
     }
-
-
-
-
-
-
-
-
-
-    render(){
-        console.log(this.state)
-        return(
-            <div>
-                <p>test run</p>
-                {this.showFood()}
-            </div>
-        
-        );
-        
-        // return();
-    }
-
-
-
 
 
 
 
 }
-
-
-
 export default FoodBox;
+
+
+
+
+
+// import React from 'react';
+// import food from '../foods.json'
+// import 'bulma/css/bulma.css';
+
+// class FoodBox extends React.Component{
+//     constructor(props){
+//         super(props);
+
+//         this.state={
+//             foodList: food,
+//         }
+//     }
+
+//     showFood = () => {
+//         return this.state.foodList.map((eachFood, index)=>{
+//             return(
+//                 <div className="box" style={{width:"600px", margin:"10px"}}>
+//   <article key={index} className="media">
+//     <div className="media-left">
+//       <figure className="image is-64x64">
+//         <img src={eachFood.image} />
+//       </figure>
+//     </div>
+//     <div className="media-content">
+//       <div className="content">
+//         <p>
+//           <strong>{eachFood.name}</strong> <br />
+//           <small>{eachFood.calories}</small>
+//         </p>
+//       </div>
+//     </div>
+//     <div className="media-right">
+//       <div className="field has-addons">
+//         <div className="control">
+//           <input
+//             className="input"
+//             type="number" 
+//             value={eachFood.quantity}
+//           />
+//         </div>
+//         <div className="control">
+//           <button className="button is-info">
+//           +
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   </article>
+// </div>
+//             )
+//         })
+//     }
+
+
+
+
+
+
+
+
+
+//     render(){
+//         console.log(this.state)
+//         return(
+//             <div>
+//                 <p>test run</p>
+//                 {this.showFood()}
+//             </div>
+        
+//         );
+        
+//         // return();
+//     }
+
+
+
+
+
+
+
+// }
+
+
+
+// export default FoodBox;
+
+
+
+
+
+
+
+
+
